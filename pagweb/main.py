3df5301b32626bf6b02405f5fd263f3ac470aa27
Nico <nicolasmuniz2005@gmail.com>
2025-02-22 18:31:05 +0100

    incorporado archivo para la funcionalidad de las paginas de respuestas

--- pagweb/main.py
+++ pagweb/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask("Pagina")
+app.secret_key = "gcedgeigia"
+
 
 def _parser(text_name:str) -> str:
     if not text_name.endswith(".txt"):
@@ -26,6 +28,7 @@ gia = []
 gei = []
 def _calc_stats(stats:list) -> list:
     p_gced, p_gia, p_gei = list(map(int, puntuaciones))
+    gced
     
         
     return stats
@@ -35,6 +38,25 @@ def _calc_stats(stats:list) -> list:
 def home() -> str:
     preguntas, puntuaciones = _parser("preguntas.txt")
     return render_template("pagina_principal.html", lista = preguntas)
+
+
+@app.route("/encuesta")
+def encuesta():
+    return render_template('encuesta.html')  # Página principal con la encuesta
+
+
+@app.route("/guardar_respuestas", methods=["POST"])
+def guardar_respuestas():
+    data = request.json
+    session["respuestas"] = data["respuestas"]  # Guardar respuestas en sesión
+    return jsonify({"status": "success"})
+
+
+@app.route("/resultados")
+def resultados():
+    respuestas = session.get("respuestas", [])
+    respuestas = list(enumerate(respuestas, start=1))  # Enumerar antes de enviarlo a la plantilla
+    return render_template("resultados.html", respuestas=respuestas)
 
 
 if __name__ == "__main__":
