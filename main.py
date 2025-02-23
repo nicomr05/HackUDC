ae35bcca6a8cb7bfc629278a298101ec89fb7c2a
Nico <nicolasmuniz2005@gmail.com>
2025-02-23 06:38:29 +0100

    Ultima version :P
    Co-authored-by: ameliegarcia <ameliegarcia@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,71 +1,77 @@
 from flask import Flask, render_template, jsonify, session, request
+from json import load
+
 
 app = Flask("Encuestas")
 app.secret_key = "xxxx"
 
+class InfoManager():
+    def __init__(self, file_name:str) -> None:
+        self.__info = self.__infoParser(file_name)
+        self.__preguntas =     [lote["Pregunta"] for lote in self.info]
+        self.__puntuaciones =   [(lote["Desacuerdo"],
+                                  lote["Indiferencia"],
+                                  lote["Acuerdo"]) for lote in self.info]
+        self.prueba_puntuaciones = [0,0,0]
+        self.respuestas = []
 
-def _parser(text_name:str) -> str:
-    if not text_name.endswith(".txt"):
-        text_name += ".txt"
+    def __infoParser(self, name:str) -> str:
+        if not name.endswith(".json"):
+            name += ".json"
 
-    with open(f"{text_name}", "r") as file:
-        bloques = file.read().split("\n\n")[:-1]
+        with open(name, "r") as json_file:
+            info = load(json_file)
 
-    for bloque in bloques:
-        info = bloque.split("\n")
-
-        preguntas = info[0]
-        codigos = info[1]
-
-        puntuaciones = tuple()
+        return info
 
 
-    return preguntas, puntuaciones, 
+    def add(self, score:str):
+        for i in range(3):
+            self.prueba_puntuaciones[i] += 1
 
 
-def _calc_stats(stats:list) -> list:
-    p_gced, p_gia, p_gei = list(map(int, stats))
-    gced += p_gced
-    gia += p_gia
-    gei += p_gei
-    #si lo hacemos así, hay que llamar a _calc_stats cuando el usuario haya respondido a todas las preguntas
+    def calculateStats(self) -> list:
+        return [i for i in self.prueba_puntuaciones]
+
+
+    @property
+    def info(self) -> list:
+        return self.__info
+
+    @property
+    def preguntas(self) -> list:
+        return self.__preguntas
     
-    return gced*0.2, gia*0.2, gei*0.2
+    @property
+    def puntuaciones(self) -> list:
+        return self.__puntuaciones
 
 
-global respuestas
+MGR = InfoManager("preguntas.json")
+
 
 @app.route("/", methods=["GET", "POST"])
 def home() -> str:
-    preguntas, puntuaciones = _parser("preguntas.txt")
 
-    if request.method == "GET":
-        prueba_puntuaciones = {"GCED":0,
-                               "GIA":0,
-                               "GEI":0}
-
-        respuestas = request.form.items()
-
-        codigo_final = []
-        for puntuacion, respuesta in zip(puntuaciones, respuestas):
-            if respuesta == "d":
-                codigo_final.append([k for k in puntuacion[0]])
-            elif respuesta == "i":
-                codigo_final.append([k for k in puntuacion[1]])
-            elif respuesta == "a":
-                codigo_final.append([k for k in puntuacion[2]])
-
-        for codigo in codigo_final:
-            prueba_puntuaciones["GCED"] += codigo[0]
-            prueba_puntuaciones["GIA"]  += codigo[1]
-            prueba_puntuaciones["GEI"]  += codigo[2]
-
-    return render_template("index.html", preguntas=preguntas, respuestas=respuestas)
+    return render_template("index.html")
 
 
 @app.route("/encuesta")
 def encuesta():
-    return render_template('encuesta.html')
+    if request.method == "GET":
+        respuestas = request.form.items()
+        respuestas = [i[1] for i in respuestas]
+        for puntuacion, respuesta in zip(MGR.puntuaciones, respuestas):
+            if respuesta == "d":
+                MGR.add(puntuacion[0])
+            elif respuesta == "i":
+                MGR.add(puntuacion[1])
+            elif respuesta == "a":
+                MGR.add(puntuacion[2])
+            
+            MGR.respuestas.append(respuesta)
+
+    return render_template('encuesta.html', preguntas=MGR.preguntas)
 
 
 @app.route("/guardar_respuestas", methods=["POST"])
@@ -77,10 +83,11 @@ def guardar_respuestas():
 
 @app.route("/resultados")
 def resultados():
-    respuestas = session.get("respuestas", [])
-    respuestas = list(enumerate(respuestas, start=1))
-    return render_template("resultados.html", respuestas=respuestas)
+    return render_template("resultados.html", estadisticas=MGR.calculateStats(), respuestas=MGR.respuestas)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5050)
+    #MGR = InfoManager("preguntas.json")
+    #MGR.add(1)
+    #print(MGR.prueba_puntuaciones)
